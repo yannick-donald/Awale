79113249d11c556e08f05878f1a765497697579b
yannick.zirihi <yannick.zirihi@boostedby.com>
2021-05-16 00:29:56 +0200

    première classes

---
+++ Classes/Joueur.py
@@ -0,0 +1,25 @@
+
+class Joueur:
+
+    def __init__(self):
+        self.nom
+        self.score
+        self.Trou
+        self.trous_possibles
+        self.adversaire
+        self.cases
+
+    def joue():
+    
+    def liste_trous():
+    
+    def ramasse_tout():
+    
+    def gagner():
+    
+    def valide_trou():
+    
+    def reste_billes():
+
+    
+        
---
+++ Classes/Trou.py
@@ -0,0 +1,20 @@
+# classe  pour un trou
+
+class Trou :
+    
+    def __init__(self, num , billes , precedent,suivant) :
+
+        self.num_trou = num
+        self.billes= billes
+        self.precedent= precedent
+        self.suivant = suivant
+    
+    def ramasse():
+    
+    def deplace():
+    
+    def deplace(nb,depart):
+
+    def valide():
+    
+    
